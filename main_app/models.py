77b8e2a5f2e2fc85f1316e589a6185208e743054
Nate Mueller <natemueller2@gmail.com>
2022-03-30 18:19:54 -0400

    added walks model

--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,11 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+TIME = (
+    ('M', 'Morning'),
+    ('A', 'Afternoon' ),
+    ('N', 'Night')
+)
 
 class Dog(models.Model):
     breed = models.CharField(max_length=200)
@@ -12,3 +17,13 @@ class Dog(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs = {'dog_id': self.id})
+
+
+
+class Walks(models.Model):
+    date = models.DateField()
+    time = models.CharField(max_length=1, choices=TIME, default=TIME[0][0])
+    dog = models.ForeignKey(Dog, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return f'{self.get_time_display()} walk on {self.date}'
