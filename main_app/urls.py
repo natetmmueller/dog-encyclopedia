a4e3ec6741b1c865874f196e6c55879bd664afbc
Nate Mueller <natemueller2@gmail.com>
2022-03-30 16:58:33 -0400

    added first form

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('dogs/', views.dog_breeds, name='index'),
-    path('dogs/<int:dog_id>/', views.dogs_detail, name = 'detail')
+    path('dogs/<int:dog_id>/', views.dogs_detail, name = 'detail'),
+    path('dogs/create', views.DogCreate.as_view(), name='dogs_create')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Dog
+from django.views.generic.edit import CreateView
 
 # Create your views here.
+
+class DogCreate(CreateView):
+    model = Dog
+    fields = ['breed', 'description', 'activity', 'image']
+    success_url = '/dogs'
 
 def home(request):
     return render(request, 'home.html')
@@ -17,3 +23,4 @@ def dog_breeds(request):
 def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
     return render(request, 'dog/detail.html', {'dog': dog})
+
